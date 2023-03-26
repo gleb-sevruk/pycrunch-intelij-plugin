db119dada01b254bceb3ea818dd7818d516da34e
Vlad Kondratenko <omgkanamikun@gmail.com>
2023-03-26 12:26:34 +0200

    added gitignore, refactored build.gradle, reviewed a few classes

--- src/main/java/icons/PycrunchCachedIcons.java
+++ src/main/java/icons/PycrunchCachedIcons.java
@@ -6,11 +6,13 @@ import javax.swing.*;
 
 // For some reason since 2021.1 icons in gutter were not cached and loaded on each appearance in the viewport
 // This will ensure the icons are not loaded every time
-public class PycrunchCachedIcons {
-    public static Icon CIRCLE_GREEN = IconLoader.getIconSnapshot(PycrunchIcons.CIRCLE_GREEN);
-    public static Icon CIRCLE_RED = IconLoader.getIconSnapshot(PycrunchIcons.CIRCLE_RED);
-    public static Icon EXCEPTION_CROSS = IconLoader.getIconSnapshot(PycrunchIcons.EXCEPTION_CROSS);
-    public static Icon CIRCLE_PROGRESS = IconLoader.getIconSnapshot(PycrunchIcons.CIRCLE_PROGRESS);
-    public static Icon CIRCLE_PENDING = IconLoader.getIconSnapshot(PycrunchIcons.CIRCLE_PENDING);
+public final class PycrunchCachedIcons {
+    public static final Icon CIRCLE_GREEN = IconLoader.getIconSnapshot(PycrunchIcons.CIRCLE_GREEN);
+    public static final Icon CIRCLE_RED = IconLoader.getIconSnapshot(PycrunchIcons.CIRCLE_RED);
+    public static final Icon EXCEPTION_CROSS = IconLoader.getIconSnapshot(PycrunchIcons.EXCEPTION_CROSS);
+    public static final Icon CIRCLE_PROGRESS = IconLoader.getIconSnapshot(PycrunchIcons.CIRCLE_PROGRESS);
+    public static final Icon CIRCLE_PENDING = IconLoader.getIconSnapshot(PycrunchIcons.CIRCLE_PENDING);
 
+    private PycrunchCachedIcons() {
+    }
 }
