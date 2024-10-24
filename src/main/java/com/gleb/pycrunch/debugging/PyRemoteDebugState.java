e57a15fb5c5e89c05352cab5096f2504be334bac
Gleb Sevruk <gleb.sevruk@databand.ai>
2024-10-24 23:01:17 +0300

    1.6.5-ce : disabled remote debugger

--- src/main/java/com/gleb/pycrunch/debugging/PyRemoteDebugState.java
+++ src/main/java/com/gleb/pycrunch/debugging/PyRemoteDebugState.java
@@ -2,44 +2,49 @@ package com.gleb.pycrunch.debugging;
 
 import com.gleb.pycrunch.shared.FreePort;
 import com.gleb.pycrunch.shared.GlobalKeys;
+import com.gleb.pycrunch.shared.IdeNotifications;
 import com.intellij.execution.Executor;
 import com.intellij.execution.RunManager;
 import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.execution.configurations.ConfigurationFactory;
 import com.intellij.execution.executors.DefaultDebugExecutor;
 import com.intellij.execution.runners.ExecutionUtil;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.project.Project;
-import com.intellij.python.pro.debugger.remote.PyRemoteDebugConfigurationParams;
-import com.intellij.python.pro.debugger.remote.PyRemoteDebugConfigurationType;
+import com.jetbrains.python.run.PythonRunConfiguration;
+import com.jetbrains.python.run.PythonConfigurationType;
 
 public class PyRemoteDebugState {
     private RunnerAndConfigurationSettings _cachedConfiguration;
 
     public void build_configuration_and_run_debugger(Project project) {
-        if (_cachedConfiguration == null) {
-            _cachedConfiguration = create_run_configuration_for_project(project);
-        }
-
-        Executor runExecutorInstance = DefaultDebugExecutor.getDebugExecutorInstance();
-        ExecutionUtil.runConfiguration(_cachedConfiguration, runExecutorInstance);
+        IdeNotifications.notify(project,"Remote debugging temporary not supported in Community Edition", "You can help with development at \n\n https://github.com/gleb-sevruk/pycrunch-intelij-plugin\n\n ", NotificationType.WARNING);
+        return;
+//        if (_cachedConfiguration == null) {
+//            _cachedConfiguration = create_run_configuration_for_project(project);
+//        }
+//
+//        Executor runExecutorInstance = DefaultDebugExecutor.getDebugExecutorInstance();
+//        ExecutionUtil.runConfiguration(_cachedConfiguration, runExecutorInstance);
     }
 
-    private RunnerAndConfigurationSettings create_run_configuration_for_project(Project project) {
-        RunnerAndConfigurationSettings settings;
-        RunManager runManager = RunManager.getInstance(project);
-
-        ConfigurationFactory factory = PyRemoteDebugConfigurationType.getInstance().getConfigurationFactories()[0];
-        settings = runManager.createConfiguration("pycrunch-engine - debugger", factory);
-
-        PyRemoteDebugConfigurationParams parameters = (PyRemoteDebugConfigurationParams) settings.getConfiguration();
-        parameters.setHost("127.0.0.1");
-
-        int port = FreePort.find_free_port();
-
-        project.putUserData(GlobalKeys.REMOTE_DEBUG_PORT_KEY, port);
-        parameters.setPort(port);
-//        settings.getConfiguration().setAllowRunningInParallel(false);
-
-        return settings;
-    }
+//    private RunnerAndConfigurationSettings create_run_configuration_for_project(Project project) {
+//        IdeNotifications.notify(project,"Remote debugging temporary not supported in Community Edition", "You can help with development at \n\n https://github.com/gleb-sevruk/pycrunch-intelij-plugin\n\n ", NotificationType.WARNING);
+//        RunnerAndConfigurationSettings settings;
+//        RunManager runManager = RunManager.getInstance(project);
+//
+//        ConfigurationFactory factory = PyRemoteDebugConfigurationType.getInstance().getConfigurationFactories()[0];
+//        settings = runManager.createConfiguration("pycrunch-engine - debugger", factory);
+////
+////        PyRemoteDebugConfigurationParams parameters = (PyRemoteDebugConfigurationParams) settings.getConfiguration();
+////        parameters.setHost("127.0.0.1");
+////
+////        int port = FreePort.find_free_port();
+////
+////        project.putUserData(GlobalKeys.REMOTE_DEBUG_PORT_KEY, port);
+////        parameters.setPort(port);
+//////        settings.getConfiguration().setAllowRunningInParallel(false);
+////
+//        return settings;
+//    }
 }
